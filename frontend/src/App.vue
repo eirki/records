da78c5ac8b7cb468a9e45af16bfe4270e726b6be
eirki <eirik.b.stavest@gmail.com>
2024-02-16 21:12:35 +0100

    Error management

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import type { Ref } from 'vue';
+import { useToast } from "vue-toastification";
 
 import Spinner from "./components/Spinner.vue";
 import Desktop from "./views/Desktop.vue"
 import Mobile from "./views/Mobile.vue"
 
 const inputData: Ref<any | null> = ref(null)
+const error: Ref<boolean> = ref(false)
+const toast = useToast();
 
 function getAlbums() {
-  return fetch(`/albums`).then(res => res.json())
+  return fetch("/albums").then(res => {
+    if (res.status === 403) {
+      error.value = true
+      toast.error("Authentication error", {
+        timeout: false,
+        closeButton: false,
+        icon: true,
+      })
+      throw new Error("API not authorized");
+    }
+    else {
+      return res.json()
+
+    }
+  })
 }
 function getAlbumsDev() {
   return import("../dev-data/library.json")
@@ -25,8 +42,8 @@ const isMobile = window.matchMedia("(any-hover: none)").matches
 </script>
 
 <template>
-  <Spinner v-if="!inputData" />
-  <component :is="isMobile ? Mobile : Desktop" v-else v-bind="inputData" />
+  <Spinner v-if="!inputData && !error" />
+  <component :is="isMobile ? Mobile : Desktop" v-else-if="!error" v-bind="inputData" />
 </template>
 
 <style>
